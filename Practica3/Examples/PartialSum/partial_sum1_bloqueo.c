f40f42b36ca44d750185856b41ffd651a649aef5
PsychO <datanc01@ucm.es>
2019-11-13 09:45:10 +0100

    si

--- Practica3/Examples/PartialSum/partial_sum1_bloqueo.c
+++ Practica3/Examples/PartialSum/partial_sum1_bloqueo.c
@@ -2,40 +2,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <semaphore.h>
-#include <math.h>
-
-typedef struct thread {
-  pthread thread_id;
-  int init_val;
-  int fin_val;
-} ThreadData;
 
 int total_sum = 0;
 sem_t sem;
 
 void * partial_sum(void * arg) {
+
+  sem_wait(&sem);
   
   int j = 0;
   int ni=((int*)arg)[0];
   int nf=((int*)arg)[1];
 
-  for (j = ni; j <= nf; j++){
-  	sem_wait(&sem);
+  for (j = ni; j <= nf; j++)
     total_sum = total_sum + j;
-    sem_post(&sem);
-  }
+
+  sem_post(&sem);
 
   pthread_exit(0);
-
 }
 
-int main(int argc, char **argv) {
+int main(/*int argc, char **argv*/void) {
   pthread_t th1, th2;
 
-  if (argc != 3) {  
-  	printf("Error parsing arguments.\n");
-  	return 1;
-  }
+  // if (argc != 2) {
+  // 	printf("Error parsing arguments.\n");
+  // 	return 1;
+  // }
+
+  // int n = argv[1];
+
+  int num1[2]={  1,   4999};
+  int num2[2]={5000, 10000};
 
   /* Create the corresponding sem */
 
@@ -51,27 +49,7 @@ int main(int argc, char **argv) {
 
   sem_init(&sem, 0, 1);
 
-  ThreadData thread_array[atoi(argv[1])];
-
-  int interval = floor(argv[2] / argv[1]);
-
-  for(int i = 0; i < argv[1]; i++){
-
-    thread_array[i].init_val = 1 + (i*interval);
-    thread_array[i].fin_val = interval + (i*interval);
-
-  }
-
-
-  for(int i = 0; i < argv[1]; i++){
-    // create the threads
-      // THE NUMBERS MUST EB PASSES AS
-        // threads_array + i
-        // cant pass by index
-  }
-
   /* Two threads are created */
-
   pthread_create(&th1, NULL, partial_sum, (void*)num1);
   pthread_create(&th2, NULL, partial_sum, (void*)num2);
 
