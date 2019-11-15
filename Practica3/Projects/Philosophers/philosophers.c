691f50139458116e59fd8bd5f8c541fa8e4faf5d
PsychO <datanc01@ucm.es>
2019-11-15 20:19:55 +0100

    they eating shit, but dunno if it's properly working tbh

--- Practica3/Projects/Philosophers/philosophers.c
+++ Practica3/Projects/Philosophers/philosophers.c
@@ -3,14 +3,20 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#include <semaphore.h> 
+
 #define NR_PHILOSOPHERS 5
+
+#define THINKING 2 
+#define HUNGRY 1 
+#define EATING 0
 
 pthread_t philosophers[NR_PHILOSOPHERS];
 pthread_mutex_t forks[NR_PHILOSOPHERS];
 
 sem_t mutex;
-sem_t S[N];
-
+sem_t S[NR_PHILOSOPHERS];
+int state[NR_PHILOSOPHERS];
 
 void init()
 {
@@ -18,6 +24,38 @@ void init()
     for(i=0; i<NR_PHILOSOPHERS; i++)
         pthread_mutex_init(&forks[i],NULL);
     
+}
+
+void take_fork(int phi){
+    sem_wait(&mutex);
+
+    printf("Philosopher %d is hungry... \n", phi);
+    state[phi] = HUNGRY;
+
+    test(phi);
+
+    sem_post(&mutex);
+    sem_wait(&S[phi]);
+
+    sleep(1);
+
+}
+
+void put_fork(int phi){
+
+    sem_wait(&mutex);
+
+    state[phi] = THINKING;
+
+    printf("Philosopher %d putting down fork %d and %d\n", phi, (phi+1) % NR_PHILOSOPHERS, (phi + NR_PHILOSOPHERS -1) % NR_PHILOSOPHERS);
+    printf("Philosopher %d is thinking\n");
+
+    think(phi);
+
+    test((phi+1) % NR_PHILOSOPHERS);
+    test((phi + NR_PHILOSOPHERS -1) % NR_PHILOSOPHERS);
+    sem_post(&mutex);
+
 }
 
 void think(int i) {
@@ -41,9 +79,18 @@ void toSleep(int i) {
     
 }
 
-void test(int i){
+void test(int phi){
 
-    if()
+    if(state[phi] == HUNGRY
+    && state[(phi+1) % NR_PHILOSOPHERS] != EATING
+    && state[(phi + NR_PHILOSOPHERS -1) % NR_PHILOSOPHERS] != EATING){
+        state[phi] = EATING;
+        sleep(2);
+        printf("Philosofer %d takes fork %d and %d\n", phi, (phi+1) % NR_PHILOSOPHERS, (phi + NR_PHILOSOPHERS -1) % NR_PHILOSOPHERS);
+        eat(phi);
+
+        sem_post(&S[phi]);
+    }
 
 }
 
@@ -57,12 +104,11 @@ void* philosopher(void* i)
         
         think(nPhilosopher);
         
-        /// TRY TO GRAB BOTH FORKS (right and left)
-
+        take_fork(nPhilosopher);
 
         eat(nPhilosopher);
         
-        // PUT FORKS BACK ON THE TABLE
+        put_fork(nPhilosopher);
         
         toSleep(nPhilosopher);
    }
@@ -77,13 +123,11 @@ int main()
     // initialize the semaphores 
     sem_init(&mutex, 0, 1); 
   
-    for (i = 0; i < N; i++) 
-        sem_init(&S[i], 0, 0); 
+    for (i = 0; i < NR_PHILOSOPHERS; i++) sem_init(&S[i], 0, 0); 
 
-    for(i=0; i<NR_PHILOSOPHERS; i++)
-        pthread_create(&philosophers[i], NULL, philosopher, (void*)i);
+    for(i=0; i<NR_PHILOSOPHERS; i++) pthread_create(&philosophers[i], NULL, philosopher, (void*)i);
     
-    for(i=0; i<NR_PHILOSOPHERS; i++)
-        pthread_join(philosophers[i],NULL);
+    for(i=0; i<NR_PHILOSOPHERS; i++) pthread_join(philosophers[i],NULL);
+    
     return 0;
 } 
